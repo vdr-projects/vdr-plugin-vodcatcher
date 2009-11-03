5ae195f9b432f99a25b6dbaf155879ce9fd815c1
etobi <git@e-tobi.net>
2009-11-03 22:03:51 +0100

    Updated copyright year and dropped $Id

--- src/SynchedDownloadPool.cc
+++ src/SynchedDownloadPool.cc
@@ -1,6 +1,6 @@
 /*
  * vdr-vodcatcher - A plugin for the Linux Video Disk Recorder
- * Copyright (c) 2007 - 2008 Tobias Grimm <vdr@e-tobi.net>
+ * Copyright (c) 2007 - 2009 Tobias Grimm <vdr@e-tobi.net>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
@@ -15,8 +15,6 @@
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * $Id: SynchedDownloadPool.cc 7652 2008-08-05 21:37:57Z svntobi $
  *
  */
 
